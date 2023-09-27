3689b7534c7032d9725f43eb7d1754b7222ef2de
CodeLHC <lhcrawford97@gmail.com>
2023-09-27 16:25:15 +0100

    submit values to add to library array

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const bookListContainer = document.getElementById("bookList");
 const newBookButton = document.getElementById("newBook");
 const dialog = document.getElementById("dialog");
+const submitButton = document.getElementById("submitButton");
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const bookPages = document.getElementById("bookPages");
+const readStatus = document.getElementsByName("readStatus");
 
-const library = ["The Hobbit", "Meditations", "Bible"];
+const library = [];
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -25,5 +30,25 @@ function generateBookList(array) {
   });
 }
 
+function getReadStatusCheckedValue() {
+  const checkedValue = Array.from(readStatus).find((radio) => radio.checked);
+  return checkedValue.value;
+}
+
+submitButton.addEventListener("click", (e) => {
+  addBookToLibrary(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    getReadStatusCheckedValue()
+  );
+  dialog.close();
+  console.log(library);
+  e.preventDefault();
+});
+
+newBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
 generateBookList(library);
-// dialog.show();
